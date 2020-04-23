5c74a69810d86de540398d2d866464525c72de16
msi <msi@msi-PC>
2020-04-24 01:25:29 +0200

    resen problem sa loggom

--- src/com/skolarajak/utils/SysUtils.java
+++ src/com/skolarajak/utils/SysUtils.java
@@ -1,13 +1,15 @@
 package com.skolarajak.utils;
+import java.util.logging.Logger;
 
-import com.sun.istack.internal.logging.Logger;
 
 public class SysUtils {
 	private final static Logger LOG = Logger.getLogger(SysUtils.class.getName());
 	public static long printDuration(long startTime) {
+		LOG.fine("start time: " + startTime);
 		long endTime = System.currentTimeMillis();
 		long duration = endTime - startTime;
 		// System.err.println("----duration---->" + duration);
+		LOG.info("----duration---->" + duration);
 		return duration;
 	}
 }
