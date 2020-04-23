f255fa256c2c89d186b9848eb6bb23e2afbd5a1d
msi <msi@msi-PC>
2020-04-23 23:44:12 +0200

    logger

--- src/com/skolarajak/utils/SysUtils.java
+++ src/com/skolarajak/utils/SysUtils.java
@@ -1,10 +1,14 @@
 package com.skolarajak.utils;
 
+import com.sun.istack.internal.logging.Logger;
+
 public class SysUtils {
+	private final static Logger LOG = Logger.getLogger(SysUtils.class.getName());
 	public static long printDuration(long startTime) {
 		long endTime = System.currentTimeMillis();
 		long duration = endTime - startTime;
-		System.err.println("----duration---->" + duration);
+		// System.err.println("----duration---->" + duration);
 		return duration;
 	}
 }
+ 
